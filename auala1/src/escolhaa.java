184bcb29aeaafe4edb015cf8dcbe13fe0059d46d
Théo Rallon <theo_rallon@estudante.sesisenai.org.br>
2025-12-16 21:17:14 -0300

    switch

---
+++ auala1/src/escolhaa.java
@@ -0,0 +1,20 @@
+
+public class escolhaa {
+    void main() {
+        int opcao = 2;
+
+        switch (opcao) {
+            case 1:
+                System.out.println("Saldo");
+                break;
+            case 2:
+                System.out.println("Extrato");
+                break;
+            case 3:
+                System.out.println("Sair");
+                break;
+            default:
+                System.out.println("Opção inválida");
+        }
+    }
+}
