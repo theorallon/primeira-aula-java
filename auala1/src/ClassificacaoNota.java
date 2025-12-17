fe769753092bd271124925a7d26f095c1e9181e8
Théo Rallon <theo_rallon@estudante.sesisenai.org.br>
2025-12-16 21:09:15 -0300

    classificacao de nota

---
+++ auala1/src/ClassificacaoNota.java
@@ -0,0 +1,12 @@
+public class ClassificacaoNota {
+    void main() {
+        int nota = 85;
+
+        if (nota >= 90) System.out.println("A");
+        else if (nota >= 80) System.out.println("B");
+        else if (nota >= 70) System.out.println("C");
+        else if (nota >= 60) System.out.println("D");
+        else System.out.println("F");
+
+    }
+}
